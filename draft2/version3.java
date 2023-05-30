94c76d69f4b857d6891171dc3da87799883adba5
sbiswas40 <124083969+sbiswas40@users.noreply.github.com>
2023-05-30 13:02:07 -0400

    Create version3.java

---
+++ draft2/version3.java
@@ -0,0 +1,71 @@
+PVector m;
+int xpos;
+int ypos;
+int value = 0;
+
+void setup(){
+size(600, 600);
+xpos = int(random(width));
+ypos = int(random(height));
+}
+
+void draw(){
+background(78, 205, 240); 
+textSize(20);
+fill(0);
+text("APCSA, periodo 7", 10, 20);
+text(" \"Golpea La Rata\" ", 5, 40);
+text("Creado por Srejon Biswas", 10, 60);
+text("Tu Puntaje:", 400, 20);
+//text(score, 500, 20);
+m = new PVector(xpos, ypos);
+mole(int(m.x), int(m.y));
+}
+
+void mouseClicked() {
+if (value == 0) {
+value = 1;
+}
+
+else if ( (abs(mouseX - xpos) < 90) && (abs(mouseY - ypos) < 120))
+{
+xpos = int(random(width));
+ypos = int(random(height));
+
+}
+
+}
+
+void mole(int x, int y){
+//hole
+fill(66,58,54);
+ellipse(x,y,100,40);
+//body
+fill(175,82,45);
+rect(x-25,y-80,50,80);
+//head
+fill(175,82,45);
+ellipse(x,y-90,75,60);
+//eyes
+fill(600,600,600);
+circle(x-10,y-100,15);
+circle(x+10,y-100,15);
+fill(0,0,0);
+circle(x-10,y-100,2);
+circle(x+10,y-100,2);
+//mouth
+fill(600,600,600);
+ellipse(x,y-80,25,20);
+//nose
+fill(0,0,0);
+ellipse(x,y-83,10,5);
+//whiskers
+line(x,y-83,x+10,y-79);
+line(x,y-83,x-10,y-79);
+line(x,y-83,x-10,y-79+4);
+line(x,y-83,x+10,y-79+4);
+//ears
+fill(175,82,45);
+ellipse(x+40,y-90,8,11);
+ellipse(x-40,y-90,8,11);
+}
